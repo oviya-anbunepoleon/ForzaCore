6af403417f52ec41c469acfc78c11cfc65994f97
oviya-anbunepoleon <harirudhra08@gmail.com>
2026-03-04 08:09:55 +0530

    fixed ai engine api key

--- backend/ai_engine.py
+++ backend/ai_engine.py
@@ -1,3 +1,4 @@
+import os
 from langchain_openai import ChatOpenAI
 
 
@@ -10,47 +11,48 @@ def generate_ai_report(
     problem_solving
 ):
     try:
-        # Initialize LLM
+
         llm = ChatOpenAI(
             model="gpt-3.5-turbo",
+            openai_api_key=os.getenv("OPENAI_API_KEY"),
             temperature=0.7
         )
 
-        # Create prompt
         prompt = f"""
-🚀 Career Intelligence Report
+Career Intelligence Report
 
 Role: {role}
 
-🔥 Strengths:
-- Analytical: {analytical}
-- Creative: {creative}
-- Communication: {communication}
-- Coding: {coding}
-- Problem Solving: {problem_solving}
+Strengths:
+Analytical: {analytical}
+Creative: {creative}
+Communication: {communication}
+Coding: {coding}
+Problem Solving: {problem_solving}
 
-📈 6 Month Roadmap:
+6 Month Roadmap
 Month 1-2: Strengthen core fundamentals
 Month 3-4: Build 2 real-world projects
 Month 5: Learn deployment & optimization
 Month 6: Build portfolio & apply to internships
 
-🛠 Recommended Tools:
-- Python
-- Git & GitHub
-- FastAPI
-- SQL / MongoDB
-- Docker
+Recommended Tools
+Python
+Git & GitHub
+FastAPI
+SQL / MongoDB
+Docker
 
-💡 Suggested Projects:
-- AI Resume Analyzer
-- Real-time Chat Application
-- Data Dashboard
+Suggested Projects
+AI Resume Analyzer
+Real-time Chat Application
+Data Dashboard
 
 Keep it structured, clear, and motivating.
 """
 
         response = llm.invoke(prompt)
+
         return response.content
 
     except Exception as e:
